ea505db5d0b97e707f9cf9a85bae76e0bf4ccf94
Kévio Castro <kevio@pactosolucoes.com.br>
2021-07-30 12:06:38 -0300

    method

--- pessoas.js
+++ pessoas.js
@@ -1,5 +1,7 @@
 const fetch = require("node-fetch");
 
-fetch("https://randomuser.me/api/")
+fetch("https://randomuser.me/api/", {
+  method: "GET",
+})
   .then((res) => res.json())
   .then((json) => console.log("Response persons changed test", json));
